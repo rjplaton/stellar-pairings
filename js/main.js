9d418c43752f0a4eb84e776e41a3543588fedf78
rjplaton <rjplaton@gmail.com>
2019-08-17 09:53:48 -0700

    initial steps to have js that toggles based on currency

--- js/main.js
+++ js/main.js
@@ -1,7 +1,81 @@
+let ccKey = ''
 let btcPrice = 147346;
 let ethPrice = 2740;
 let ltcPrice = 1173;
 let usdPrice = 13;
+
+let xmlRequest = new XMLHttpRequest();
+//xmlRequest.open('GET', 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=XLM&' + 'api_key' + ccKey, true);
+xmlRequest.open('GET', 'XLMprice.json', true);
+
+
+//consider an array/dict for btc, ltc, etc
+xmlRequest.onload = function() {
+	let data = JSON.parse(this.response)
+ 	if (xmlRequest.status != 200) { // analyze HTTP status of the response
+    	console.log(`Error ${xmlRequest.status}: ${xmlRequest.statusText}`); // e.g. 404: Not Found
+  		console.log(data.XLM)
+  		let btcPrice = data.XLM;
+  	} else { // show the result
+    	console.log(`Done, got ${xmlRequest.response.length} bytes`); // responseText is the server
+  	}
+};
+
+xmlRequest.send();
+
+
+console.log(btcPrice);
+
+
+//render function
+//array of currencies
+currencies = [
+	{
+	name:'Bitcoin',
+	abrev: 'btc',
+	price: 150000,
+	},
+	{
+	name: 'US Dollar',
+	abrev: 'usd',
+	price: 13,
+	},
+]
+//pseudocode for future checkboxes to add/remove barChart-rows
+function test() {
+
+	// Fetch the div from the page
+    let barChartDiv = document.querySelector('.BarChart');
+    barChartDiv.innerHTML = '';
+
+	for (let currency of currencies) {
+		//get the values from array of currencies
+		let currencyName = currency.name;
+		let xlmValue = currency.price;
+		let currencyAbrev = currency.abrev;
+		
+		//create a bar div for the currency
+		let newRowDiv = document.createElement('div');
+		newRowDiv.classList.add('BarChart-row');
+		
+		let newBarDescDiv = document.createElement('div');
+		newBarDescDiv.classList.add('BarChart-rowdesc');
+		newBarDescDiv.textContent = '1 ' + currencyName + ' (' + currencyAbrev.toUpperCase() + ')';
+
+		let newBarDiv = document.createElement('div');
+		newBarDiv.classList.add('BarChart-bar');
+		newBarDiv.id = currencyAbrev + 'bar';
+		newBarDiv.style.width = 500 + 'px';
+
+		//add the divs to the page
+		barChartDiv.appendChild(newRowDiv);
+		newRowDiv.appendChild(newBarDescDiv);
+		newRowDiv.appendChild(newBarDiv);
+
+		console.log(currencyName + 'bar created.');
+	};
+};
+
 
 
 function barClick(clickedID) {
